c9e7279ffcde5b40653ff36e8d490982d98a7e92
Dave Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2026-08-02 14:02:43 -0500

    Create main.py

---
+++ ESP32/hace_buen_tiempo/main.py
@@ -0,0 +1,6 @@
+# A MicroPython program for the ESP32-S3 SuperMini and an SHT3x temperature sensor
+# Display a mood color on the built-in NeoPixel depending on the temperature.
+
+# Requires SHT3x module: https://github.com/DavesCodeMusings/SHT3x-MicroPython
+
+# TODO: Create the program based on what you learn from the example code.
