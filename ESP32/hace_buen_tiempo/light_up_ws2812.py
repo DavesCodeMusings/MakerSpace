037df6937227cf3ff5e01f27acb51b5b32ac79df
Dave Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2026-08-02 14:54:21 -0500

    Update light_up_ws2812.py

--- ESP32/hace_buen_tiempo/light_up_ws2812.py
+++ ESP32/hace_buen_tiempo/light_up_ws2812.py
@@ -1,10 +1,13 @@
+# Demo program to light up different colors on a single WS2812 NeoPixel,
+# like the one on the ESP32-S3 SuperMini.
+
 from micropython import const
 from machine import Pin
 from neopixel import NeoPixel
 from time import sleep
 
-GPIO = const(48)  # Change if needed to match microcontroller.
 
+GPIO = const(48)  # Change if needed to match microcontroller.
 RED = const((63, 0, 0))
 GREEN = const((0, 63, 0))
 BLUE = const((0, 0, 63))
