c193fb6d97750988fe3f028081bc87e998359be5
Dave Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2026-08-02 14:04:02 -0500

    Create i2c_bus_scan.py

---
+++ ESP32/hace_buen_tiempo/i2c_bus_scan.py
@@ -0,0 +1,18 @@
+# Scan for I2C devices and print addresses found
+
+from machine import Pin, SoftI2C
+
+i2c_clock = 4
+i2c_data = 5
+
+i2c = SoftI2C(scl=Pin(i2c_clock), sda=Pin(i2c_data))
+
+print('Scanning i2c bus...')
+devices = i2c.scan()
+
+if (len(devices) == 0):
+    print("No i2c devices.")
+else:
+    print('Devices found:', len(devices))
+    for address in devices:  
+        print("Address: ", hex(address))
